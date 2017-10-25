e8c387b3c45f04ee1c9dc6f0c4379ef9c63c37d8
Jeff Ferrell <jwferrell78@gmail.com>
2017-10-25 01:06:44 -0700

    ViewState, Arrays, and Iteration Statements exercise
    Used ViewState to initialize arrays and made iterative statements to resize arrays by adding index slots, then sent the new arrays back to ViewState.

---
+++ ChallengeEpicSpiesAssetTracker/SpyAssetTracker.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="SpyAssetTracker.aspx.cs" Inherits="ChallengeEpicSpiesAssetTracker.SpyAssetTracker" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 119px;
+            height: 150px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div style="font-family: Arial, Helvetica, sans-serif">
+    
+        <img alt="Epic Spies Logo" class="auto-style1" src="epic-spies-logo.jpg" /><h3>Asset Performance Tracker</h3>
+    
+    </div>
+        <p>
+            Asset Name:
+            <asp:TextBox ID="assetNameTextBox" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            Elections Rigged:
+            <asp:TextBox ID="electionsRiggedTextBox" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            Acts of Subterfuge Performed:
+            <asp:TextBox ID="actsOfSubterfugeTextBox" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            <asp:Button ID="addButton" runat="server" OnClick="addButton_Click" Text="Add Asset" />
+        </p>
+        <p>
+            <asp:Label ID="performanceLabel" runat="server"></asp:Label>
+        </p>
+    </form>
+</body>
+</html>
---
+++ ChallengeEpicSpiesAssetTracker/SpyAssetTracker.aspx.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeEpicSpiesAssetTracker
+{
+    public partial class SpyAssetTracker : System.Web.UI.Page
+    {
+        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                string[] assetNames = new string[0];
+                ViewState.Add("assets", assetNames);               
+
+                int[] electionsRigged = new int[0];
+                ViewState.Add("elections", electionsRigged);                
+
+                double[] actsOfSubterfuge = new double[0];
+                ViewState.Add("subterfuge", actsOfSubterfuge);                
+            }
+        }
+
+        protected void addButton_Click(object sender, EventArgs e)
+        {
+            string[] assetNames = (string[])ViewState["assets"];
+            Array.Resize(ref assetNames, assetNames.Length + 1);
+            int newAsset = assetNames.GetUpperBound(0);
+            assetNames[newAsset] = assetNameTextBox.Text;
+            string latestAsset = assetNames[newAsset];
+            ViewState["assets"] = assetNames;
+            assetNameTextBox.Text = "";
+
+            int[] electionsRigged = (int[])ViewState["elections"];
+            Array.Resize(ref electionsRigged, electionsRigged.Length + 1);
+            int newElectionsRigged = electionsRigged.GetUpperBound(0);
+            electionsRigged[newElectionsRigged] = int.Parse(electionsRiggedTextBox.Text);
+            ViewState["elections"] = electionsRigged;
+            electionsRiggedTextBox.Text = "";
+
+            double[] actsOfSubterfuge = (double[])ViewState["subterfuge"];
+            Array.Resize(ref actsOfSubterfuge, actsOfSubterfuge.Length + 1);
+            int newActsOfSubterfuge = actsOfSubterfuge.GetUpperBound(0);
+            actsOfSubterfuge[newActsOfSubterfuge] = double.Parse(actsOfSubterfugeTextBox.Text);
+            ViewState["subterfuge"] = actsOfSubterfuge;
+            actsOfSubterfugeTextBox.Text = "";
+
+            performanceLabel.Text = String.Format
+                ("Total Elections Rigged: {0} <br/> Average Acts of Subterfuge per Asset: {1:N2} <br/> (Last Asset you Added: {2})",
+                electionsRigged.Sum(),
+                actsOfSubterfuge.Average(),
+                latestAsset);
+
+        }
+    }
+}
